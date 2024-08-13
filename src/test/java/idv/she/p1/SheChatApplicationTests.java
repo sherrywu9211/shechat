83c6880fa6ee71c80335c46bf1d162fe56bc344a
Sherry <hsuan.wu9211@gmail.com>
2024-08-13 18:10:54 +0800

    新建專案&設定相關檔案(設定DB連線資料&.gitgnore)

---
+++ src/test/java/idv/she/p1/SheChatApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.she.p1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
